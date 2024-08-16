17e50b515b331d45d7a8365f7d98fbe8b86f7a9e
Mehriddin <106508108+Mehriddin1397@users.noreply.github.com>
2024-08-16 13:08:21 +0500

    inheritance

--- index.php
+++ index.php
@@ -20,8 +20,11 @@ $Dell = new Computer();
 $Dell->protsessor = 'Intel Core i7'; */
 
 
+
+
+
 //constructor
-class Computer {
+/** class Computer {
 
     	//atribut
     public $protsessor;
@@ -45,6 +48,48 @@ class Computer {
 
 $dell = new Computer('core i7','16gb');
 
-var_dump($dell->charge());
+var_dump($dell->charge()); */
 
+
+
+//Inheritance(meros qilib olish)
+//extends(kelib chiqadi)
+//overriding (ustidan yozvorish)
+//final (oxiri, u classdan extends qilib bumaydi, buni methodlarga ham qullasa buladi) 
+
+class animal{
+    //atributs
+	public $foots;
+	public $weight;
+
+    //methots
+	public function speed(){
+		echo "4 km/s";
+	}
+
+    //construct
+	public function __construct($foots,$weight){
+		$this->foots = $foots;
+		$this -> weight = $weight;
+	}
+}
+
+class tiger extends animal{
+	public $color ;
+
+	//methods
+	public function type(){
+		echo "predator";
+	}
+	
+	//overriding
+	public function speed(){
+		echo "50 km/s";
+	}
+}
+
+
+$mountain_tiger = new tiger(4,100);
+
+var_dump($mountain_tiger->speed());
 
