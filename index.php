09005198c0a807b10e0a4ba4fc0d548209192143
Mehriddin <106508108+Mehriddin1397@users.noreply.github.com>
2024-08-16 12:12:00 +0500

    constuct

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-
-class Computer {
+// class 
+/** class Computer {
 
     public $protsessor;
     public $ozu;
@@ -17,6 +17,34 @@ class Computer {
 }
 
 $Dell = new Computer();
-$Dell->protsessor = 'Intel Core i7';
-print_r($Dell->color('black'));
-echo $Dell->setModel('Inspiron 14');
+$Dell->protsessor = 'Intel Core i7'; */
+
+
+//constructor
+class Computer {
+
+    	//atribut
+    public $protsessor;
+    public $ozu;
+
+
+	//methods
+    public function charge()
+	{
+    	echo "Minimal 4 soat";
+	}
+
+
+	//constracts
+    public function __construct($protsessor,$ozu)
+	{
+    	$this-> protsessor = $protsessor;
+	$this-> ozu = $ozu;
+	}    
+}
+
+$dell = new Computer('core i7','16gb');
+
+var_dump($dell->charge());
+
+
